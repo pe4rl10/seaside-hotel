948f631bf76bb8c0e501340c70756763506a3619
Pearl Patel <pearlpatel052@gmail.com>
2024-03-06 02:34:19 +0530

    Footer and NavBar added

--- client/seaSide-hotel/src/App.jsx
+++ client/seaSide-hotel/src/App.jsx
@@ -8,6 +8,8 @@ import ExistingRooms from "./components/room/ExistingRooms";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import EditRoom from "./components/room/EditRoom";
+import NavBar from "./components/layout/NavBar";
+import Footer from "./components/layout/Footer";
 
 function App() {
 
@@ -15,6 +17,7 @@ function App() {
         <>
             <main>
                 <Router>
+                    <NavBar/>
                     <Routes>
                         <Route path="/" element={<Home/>}/>
                         <Route path="/edit-room/:roomId" element={<EditRoom/>}/>
@@ -22,6 +25,7 @@ function App() {
                         <Route path="/add-room" element={<AddRoom/>}/>
                     </Routes>
                 </Router>
+                <Footer/>
             </main>
         </>
     
