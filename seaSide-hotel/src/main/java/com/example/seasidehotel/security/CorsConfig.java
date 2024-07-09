076d89fe5c520ba472907a7396b8105bcf7fc51f
Pearl Patel <pearlpatel052@gmail.com>
2024-07-09 20:41:27 +0530

    CORS origin setup

--- seaSide-hotel/src/main/java/com/example/seasidehotel/security/CorsConfig.java
+++ seaSide-hotel/src/main/java/com/example/seasidehotel/security/CorsConfig.java
@@ -24,7 +24,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://seaside-hotel-black.vercel.app/");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
