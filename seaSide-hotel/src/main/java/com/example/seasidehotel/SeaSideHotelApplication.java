d2745e95ffb392c433539241bd12549fc86a45d6
Pearl Patel <pearlpatel052@gmail.com>
2024-10-19 14:13:26 +0530

    Final Configuration for deployment on railway

--- seaSide-hotel/src/main/java/com/example/seasidehotel/SeaSideHotelApplication.java
+++ seaSide-hotel/src/main/java/com/example/seasidehotel/SeaSideHotelApplication.java
@@ -1,15 +1,20 @@
 package com.example.seasidehotel;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
 public class SeaSideHotelApplication {
 
     public static void main(String[] args) {
+        // Load the .env file and set environment variables
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+
+        // Start the Spring Boot application
         SpringApplication.run(SeaSideHotelApplication.class, args);
     }
-
 }
